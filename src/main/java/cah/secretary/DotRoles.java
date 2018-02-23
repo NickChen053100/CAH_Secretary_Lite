8c28119dff3d79cf75c6495405ff96ebae2b8f32
Nick Chen <nick.chen053100@gmail.com>
2018-02-22 19:36:54 -0600

    Fixed an issue with the .roles command where it would show roles that are not self-assignable

--- src/main/java/cah/secretary/DotRoles.java
+++ src/main/java/cah/secretary/DotRoles.java
@@ -6,6 +6,7 @@ import net.dv8tion.jda.core.entities.Role;
 import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.core.hooks.ListenerAdapter;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class DotRoles extends ListenerAdapter {
@@ -17,10 +18,36 @@ public class DotRoles extends ListenerAdapter {
         if (content.equals(".roles")) {
             MessageChannel channel = event.getChannel();
             List<Role> rolesList = event.getGuild().getRoles();
-            //channel.sendMessage(String.valueOf(rolesList.size())).queue(); //test line
-            String roles = rolesList.get(0).getName();
-            for (int i = 1; i < rolesList.size(); i++) {
-                roles += ", " + rolesList.get(i).getName();
+            //List of roles that aren't self assignable
+            List<String> invalidRolesList = new ArrayList<>();
+            invalidRolesList.add("Admin");
+            invalidRolesList.add("Dyno");
+            invalidRolesList.add("UB3R-B0T");
+            invalidRolesList.add("Moderator");
+            invalidRolesList.add("Partner");
+            invalidRolesList.add("Admin Emeritus");
+            invalidRolesList.add("Mod Emeritus");
+            invalidRolesList.add("Muted");
+            invalidRolesList.add("Verified");
+            invalidRolesList.add("Committed");
+            invalidRolesList.add("Accepted");
+            invalidRolesList.add("Admissions");
+            invalidRolesList.add("Contributor");
+            invalidRolesList.add("Pulitzer Winner");
+            invalidRolesList.add("Bot Commander");
+            invalidRolesList.add("Bot");
+            invalidRolesList.add("ApplyingToCollegeBot");
+            invalidRolesList.add("Cyan");
+            invalidRolesList.add("(ﾉ◕ヮ◕)ﾉ✧･ﾟ*✧spoo.py✧*･ﾟ✧ヽ(◕ヮ◕)ﾉ");
+            invalidRolesList.add("Tatsumaki");
+            invalidRolesList.add("MathBot");
+            invalidRolesList.add("Botless");
+            invalidRolesList.add("IT Guy");
+            invalidRolesList.add("@everyone");
+            String roles = "| ";
+            for (int i = 0; i < rolesList.size(); i++) {
+                if (!(invalidRolesList.contains(rolesList.get(i).getName())))
+                    roles += rolesList.get(i).getName() + " | ";
             }
             channel.sendMessage("The following roles are available for self-assigning: ```" + roles + "```").queue(); // Important to call .queue() on the RestAction returned by sendMessage(...)
         }
